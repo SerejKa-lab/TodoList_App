d1e8a388970e6f214052e78453d4f00970b210b7
SerejKa-lab <seruogacom@mail.ru>
2019-10-08 12:21:26 +0300

    TodoList project start

---
+++ src/TodoListFooter.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class TodoListFooter extends React.Component {
+    render = () => {
+        return (
+            
+        );
+    }
+}
+
+export default TodoListFooter;
+
---
+++ src/TodoListHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class TodoListHeader extends React.Component {
+    render = () => {
+        return (
+            <
+        );
+    }
+}
+
+export default TodoListHeader;
+
