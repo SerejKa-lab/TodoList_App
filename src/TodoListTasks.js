5728f98de09fbda553f652f6fa7fac847c79948e
SerejKa-lab <seruogacom@mail.ru>
2019-10-08 14:03:29 +0300

    Transformation TodoListTask by MAP method

--- src/TodoListTasks.js
+++ src/TodoListTasks.js
@@ -1,25 +1,31 @@
 import React from 'react';
+import TodoListTask from './TodoListTask';
 
 class TodoListTasks extends React.Component {
     render = () => {
+
+        let subject = [
+            { title: "HTML", isDone: true },
+            { title: 'CSS', isDone: true },
+            { title: 'JS', isDone: true },
+            { title: 'React', isDone: false }
+        ];
+
+        let subjectElements = subject.map((subj) => {
+            return <TodoListTask title={subj.title} isDone={subj.isDone} />
+
+        });
+
+        // let subjectElementsFalse = [
+        //     <TodoListTask title={subject[0].title} isDone={subject[0].isDone} />,
+        //     <TodoListTask title={subject[1].title} isDone={subject[1].isDone} />,
+        //     <TodoListTask title={subject[2].title} isDone={subject[2].isDone} />,
+        //     <TodoListTask title={subject[3].title} isDone={subject[3].isDone} />
+        // ];
+
         return (
-            <div className="todoList-tasks">
-                <div className="todoList-task">
-                    <input type="checkbox" checked={true} />
-                    <span>CSS</span>
-                </div>
-                <div className="todoList-task">
-                    <input type="checkbox" checked={false} />
-                    <span>JS</span>
-                </div>
-                <div className="todoList-task">
-                    <input type="checkbox" checked={false} />
-                    <span>ReactJS</span>
-                </div>
-                <div className="todoList-task">
-                    <input type="checkbox" checked={true} />
-                    <span>Patterns</span>
-                </div>
+            <div className='todoList-Tasks'>
+                {subjectElements}
             </div>
         );
     }
