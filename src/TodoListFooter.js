27f0eedb725eeec8e95af7d0e8b4300744db5f06
SerejKa-lab <seruogacom@mail.ru>
2019-10-08 12:30:23 +0300

    Task1: 'Todo' react components are created

--- src/TodoListFooter.js
+++ src/TodoListFooter.js
@@ -3,7 +3,11 @@ import React from 'react';
 class TodoListFooter extends React.Component {
     render = () => {
         return (
-            
+            <div className="todoList-footer">
+                <button>All</button>
+                <button>Completed</button>
+                <button>Active</button>
+            </div>
         );
     }
 }
