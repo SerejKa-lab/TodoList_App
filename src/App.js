bb9293438f791d9a683120991b045a438939d9d1
SerejKa-lab <seruogacom@mail.ru>
2019-11-28 13:13:07 +0300

    addListItem, addTaskItem; AddItemForm in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import TodoList from './TodoList';
+import AddItemForm from './AddItemForm';
 
 
 class App extends React.Component {
@@ -9,6 +10,7 @@ class App extends React.Component {
     };
 
     state = {
+        nextListId: 5,
         lists: []
         // lists: [
         //     { id: 0, title:  'Спорт', tasks: [{ id: 1, title: 'CSS', isDone: false, priority: 'medium' }], nextTaskId: 2 },
@@ -23,27 +25,33 @@ class App extends React.Component {
     }
 
     restoreState = () => {
-        debugger;
-        let state = {lists:[]};
+        let state = { lists: [] };
         const storageState = localStorage.getItem('TodoLists');
         if (storageState !== null) {
-            state = JSON.parse( storageState )
+            state = JSON.parse(storageState)
         };
         this.setState(state);
     }
 
-    addTask = (listId, taskTitle) => {
-        const taskId = this.state.lists[listId].nextTaskId;
-        const newTask = { id: taskId, title: taskTitle, isDone: false, priority: 'medium' }
-        const newLists = this.state.lists.map( (list) => {
-            return (list.id === listId)
-            ? { ...list, tasks: [ ...list.tasks, newTask ], nextTaskId: list.nextTaskId+1 }
-            : list
-        } );
-        this.setState( { lists: newLists }, this.saveState );
+    addListItem = ( listTitle ) => {
+        const listId = this.state.nextListId;
+        const newList = { id: listId, title: listTitle, tasks: [], nextTaskId: 1 };
+        this.setState( { lists: [ ...this.state.lists, newList ], nextListId: listId+1 } )
     }
 
-    
+    addTaskItem = (taskTitle, listId) => {
+        const listIndex = this.state.lists.findIndex( (list) => list.id === listId );
+        const taskId = this.state.lists[listIndex].tasks.length + 1;
+        const newTask = { id: taskId, title: taskTitle, isDone: false, priority: 'medium' }
+        const newLists = this.state.lists.map((list) => {
+            return (list.id === listId)
+                ? { ...list, tasks: [...list.tasks, newTask], nextTaskId: list.nextTaskId + 1 }
+                : list
+        });
+        this.setState({ lists: newLists }, this.saveState);
+    }
+
+
     changeTask = (listId, taskId, dataObj) => {
         const newLists = this.state.lists.map((list) => {
             if (list.id === listId) {
@@ -57,23 +65,26 @@ class App extends React.Component {
                 }
             } else return list;
         })
-        this.setState({ lists: newLists }, this.saveState );
+        this.setState({ lists: newLists }, this.saveState);
     }
-    
+
 
     render() {
         
-        const todoLists = this.state.lists.map( (list) => 
-            <TodoList 
-                list = { list } 
-                key = { list.id }
-                addTask = { this.addTask }
-                changeTask = {this.changeTask} />
+        const todoLists = this.state.lists.map((list) =>
+            <TodoList
+                list={list}
+                key={list.id}
+                addItem={this.addTaskItem}
+                changeTask={this.changeTask} />
         )
 
         return (
-            <div className='App'>
-               { todoLists } 
+            <div className='app'>
+                <AddItemForm addItem = { this.addListItem }/>
+                <div className='app_lists'>
+                    {todoLists}
+                </div>
             </div>
         )
     }
