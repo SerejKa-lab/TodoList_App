fe234d84bc31a45ce900c4540b5006acd8269a88
SerejKa-lab <seruogacom@mail.ru>
2019-11-29 14:32:55 +0300

    save srote.state to localStorage, listId are modified

--- src/store.js
+++ src/store.js
@@ -2,13 +2,15 @@ import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 
-const initialState = {
-    nextListId: 2,
-    lists: 
-        [{ id: 0, title:  'Спорт',  nextTaskId: 2, tasks: [{ id: 1, title: 'CSS', isDone: false, priority: 'medium' }] },
-         { id: 1, title:  'Спорт',  nextTaskId: 2, tasks: [{ id: 1, title: 'CSS', isDone: false, priority: 'medium' }] }
-        ] 
-};
+const initialState = localStorage.getItem('TodoListForGit') !== null
+    ? JSON.parse(localStorage.getItem('TodoListForGit'))
+    : {
+        nextListId: 1,
+        lists: []
+        //lists: [{id: 0, title: 'Спорт', nextTaskId: 2,
+        //     tasks: [{ id: 1, title: 'CSS', isDone: false, priority: 'medium' }]  }]
+    };
+
 
 
 const reducer = (state = initialState, action) => {
@@ -16,53 +18,55 @@ const reducer = (state = initialState, action) => {
     switch (action.type) {
 
         case 'ADD-LIST':
-            const listId = state.lists.length === 0 ? 0 : state.nextListId;
+            const listId = state.lists.length;
             const newList = { id: listId, title: action.listTitle, tasks: [], nextTaskId: 1 };
-            return { 
+            return {
                 ...state,
-                lists: [...state.lists, newList], 
-                nextListId: listId + 1 
+                lists: [...state.lists, newList],
+                nextListId: listId + 1
             }
-        
+
         case 'DELETE-LIST':
             return {
                 ...state,
-                lists: state.lists.filter( (list) => list.id !== action.listId )
+                lists: 
+                    state.lists.filter((list) => list.id !== action.listId)
+                    .map( (list, index) => ({ ...list, id: index }) )
             }
-            
+
         case 'ADD-TASK':
             const taskId = state.lists[action.listId].tasks.length + 1;
-            const newTask = { id: taskId, title: action.taskTitle, isDone: false, priority:'medium' }
+            const newTask = { id: taskId, title: action.taskTitle, isDone: false, priority: 'medium' }
             return {
                 ...state,
-                lists: state.lists.map( (list) => {
+                lists: state.lists.map((list) => {
                     if (list.id === action.listId) {
                         return {
                             ...list,
                             nextTaskId: list.nextTaskId + 1,
-                            tasks: [ ...list.tasks, newTask ]
+                            tasks: [...list.tasks, newTask]
                         }
                     } else return list;
-                    
-                } )
+
+                })
             }
 
         case 'DELETE-TASK':
             return {
                 ...state,
-                lists: state.lists.map( (list) => {
+                lists: state.lists.map((list) => {
                     if (list.id === action.listId) {
                         return {
                             ...list,
-                            tasks: 
-                                list.tasks.filter( (task) => task.id !== action.taskId )
-                                    .map( (task, index) => ({ ...task, id: index+1 }) )
-                                    // получаю список заданий с номерами по порядку
+                            tasks:
+                                list.tasks.filter((task) => task.id !== action.taskId)
+                                    .map((task, index) => ({ ...task, id: index + 1 }))
+                            // получаю список заданий с номерами по порядку
                         }
                     } else return list
-                } )
+                })
             }
-        
+
         case 'UPDATE-TASK':
 
             const getNewPriorityObj = () => {
@@ -80,18 +84,18 @@ const reducer = (state = initialState, action) => {
 
             return {
                 ...state,
-                lists: state.lists.map( (list) => {
-                    if ( list.id === action.listId ) {
+                lists: state.lists.map((list) => {
+                    if (list.id === action.listId) {
                         return {
                             ...list,
-                            tasks: list.tasks.map( (task) => {
+                            tasks: list.tasks.map((task) => {
                                 if (task.id === action.taskId) {
                                     return { ...task, ...newData }
                                 } else return task
-                            } )
+                            })
                         }
                     } else return list;
-                } )
+                })
             }
 
         default: return state;
@@ -100,6 +104,6 @@ const reducer = (state = initialState, action) => {
 }
 
 
-const store = createStore( reducer, composeWithDevTools() );
+const store = createStore(reducer, composeWithDevTools());
 
 export default store;
