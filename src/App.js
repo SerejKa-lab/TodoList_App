f9dab8073a2fe32fe8775b8808dcc0ee8f890f8d
SerejKa-lab <seruogacom@mail.ru>
2019-11-29 10:17:22 +0300

    App connected to store with state and addList

--- src/App.js
+++ src/App.js
@@ -1,47 +1,12 @@
 import React from 'react';
 import TodoList from './TodoList';
 import AddItemForm from './AddItemForm';
+import { connect } from 'react-redux';
 
 
 class App extends React.Component {
 
-    componentDidMount() {
-        this.restoreState();
-    };
-
-    state = {
-        nextListId: 1,
-        lists: []
-        // lists: [
-        //     { id: 0, title:  'Спорт', tasks: [{ id: 1, title: 'CSS', isDone: false, priority: 'medium' }], nextTaskId: 2 },
-        // ]
-    };
-
-    saveState = () => {
-        const stateToString = JSON.stringify(this.state);
-        localStorage.setItem('TodoLists', stateToString);
-    }
-
-    restoreState = () => {
-        let state = { lists: [] };
-        const storageState = localStorage.getItem('TodoLists');
-        if (storageState !== null) {
-            state = JSON.parse(storageState)
-        };
-        this.setState(state);
-    }
-
-    addList = ( listTitle ) => {
-        const listId = this.state.lists.length === 0 ? 0 : this.state.nextListId;
-        const newList = { id: listId, title: listTitle, tasks: [], nextTaskId: 1 };
-        this.setState( { lists: [ ...this.state.lists, newList ], nextListId: listId+1 }, this.saveState )
-    }
-
-    deleteList = ( listId ) => {
-        const newLists = this.state.lists.filter( (list) => list.id !== listId );
-        this.setState( { lists: newLists }, this.saveState );
-    }
-
+  
     addTask = (taskTitle, listId) => {
         const listIndex = this.state.lists.findIndex( (list) => list.id === listId );
         const taskId = this.state.lists[listIndex].tasks.length + 1;
@@ -74,19 +39,19 @@ class App extends React.Component {
 
     render() {
         
-        const todoLists = this.state.lists.map((list) =>
+        const todoLists = this.props.lists.map((list) =>
             <TodoList
                 list={list}
                 key={list.id}
+                
                 addItem={this.addTask}
-                changeTask={this.changeTask}
-                deleteList={this.deleteList} />
+                changeTask={this.changeTask} />
         )
 
         return (
             <div className='app'>
                 <h2>Органайзер задач</h2>
-                <AddItemForm addItem = { this.addList } placeholder = 'Add list'/>
+                <AddItemForm addItem = { this.props.addList } placeholder = 'Add list'/>
                 <div className='app_lists'>
                     {todoLists}
                 </div>
@@ -95,4 +60,22 @@ class App extends React.Component {
     }
 }
 
-export default App;
+const mapStateToProps = ( state ) => {
+    return {
+        lists: state.lists
+    }
+}
+
+const mapDispatchToProps = ( dispatch ) => {
+    return {
+        addList: ( listTitle ) => {
+            const action = {
+                type: 'ADD-LIST',
+                listTitle: listTitle
+            }
+            dispatch( action );
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
