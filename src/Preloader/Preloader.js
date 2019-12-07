9dc1e9f0983982ecb86afa605cbd6355a7db69cf
SerejKa-lab <seruogacom@mail.ru>
2019-12-07 22:06:29 +0300

    Preloader margin-left, inProgress style for loading indicator items

--- src/Preloader/Preloader.js
+++ src/Preloader/Preloader.js
@@ -2,8 +2,12 @@ import React from 'react';
 import preloader from './preloader.svg';
 
 
-const Preloader = ({ width='30px', height='10px' }) => {
-    const imgStyle = {width: width, height: height};
+const Preloader = ({ width='30px', height='15px' }) => {
+    const imgStyle = {
+        width: width, 
+        height: height,
+        'margin-left': '10px'
+    };
       
     return(
         <img style={imgStyle} src={preloader} alt='preloader'/>
