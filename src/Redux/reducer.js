57e94ab31c625801e9f521343b90c11ab75c61c8
SerejKa-lab <seruogacom@mail.ru>
2020-02-04 23:15:08 +0300

    thunk refactoring in reducer

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -1,10 +1,15 @@
+import {api} from '../API/api'
 
 const initialState =  {
-    lists: []
+    lists: [],
+    listsProgress: { listsLoading: false, tasksLoading: false },
+    maxListsCount: 10
     /* lists: [
         {
-            id: 0, title: 'Спорт', nextTaskId: 2, totalCount: 1, countOnPage: 10,
-            tasks: [{ id: 1, title: 'CSS', completed: false, priority: 'medium' }]
+            id: 0, title: 'Спорт', nextTaskId: 2, totalCount: 1, countOnPage: 10, filterValue: 'All',
+            page: 1, tasks: [{ id: 1, title: 'CSS', completed: false, priority: 'medium',
+            listInProcess: false, titleUpdating: false, taskIsAdding: false, listDeliting: false
+         }]
         }
     ] */
     };
@@ -17,18 +22,22 @@ const reducer = (state = initialState, action) => {
 
     switch (action.type) {
 
+  // ---------------------------List Reducers -------------------------------
+        
         case RESTORE_LISTS:
             return {
                 ...state,
                 lists: action.lists.map( (list) => {
                     return !list.tasks
-                        ? { ...list, page: 1, countOnPage, tasks: [] } 
-                        : { ...list, page: 1, countOnPage }
+                        ? { ...list, page: 1, countOnPage, filterValue: 'All', tasks: [],
+                            footerProcess: false } 
+                        : { ...list, page: 1, countOnPage, filterValue: 'All',
+                            footerProcess: false }
                 } )
             }
 
         case ADD_LIST:
-            const extendedList = { ...action.list, page: 1, countOnPage, tasks: [] };
+            const extendedList = { ...action.list, page: 1, countOnPage, filterValue: 'All', tasks: [] };
             return {
                 ...state,
                 lists: [ extendedList, ...state.lists ]
@@ -50,6 +59,34 @@ const reducer = (state = initialState, action) => {
                     state.lists.filter((list) => list.id !== action.listId)
             }
 
+        case LIST_IS_LOADING:
+            return {
+                ...state,
+                listsProgress: {...state.listsProgress, listsLoading: action.value}
+            }
+
+        case LIST_IN_PROCESS:
+            return {
+                ...state,
+                lists: state.lists.map((list) => {
+                    if (list.id === action.listId) return { ...list, [action.process]: action.value }
+                    else return list
+                })
+            }
+
+        case SET_FILTER_VALUE:
+            return {
+                ...state,
+                lists: state.lists.map((list) => {
+                    if (list.id === action.listId) return { ...list, filterValue: action.value }
+                    else return list
+                })
+            }
+
+
+ // ---------------------------Tasks Reducers-------------------------------
+       
+
         case RESTORE_TASKS:
             return {
                 ...state,
@@ -58,8 +95,7 @@ const reducer = (state = initialState, action) => {
                         ? {
                             ...list,
                             totalCount: action.totalCount,
-                            tasks: !action.tasks 
-                                ? [] 
+                            tasks: !action.tasks ? [] 
                                 : action.tasks.filter( (task, index) => index < countOnPage )
                                     .map( (task, index) => ({...task, renderIndex: index + 1 }) )
                         }
@@ -83,21 +119,6 @@ const reducer = (state = initialState, action) => {
                 })
             }
 
-        case ADD_TASK:
-            return {
-                ...state,
-                lists: state.lists.map((list) => {
-                    if (list.id === action.task.todoListId) {
-                        return {
-                            ...list,
-                            totalCount: list.totalCount + 1,
-                            tasks: [action.task, ...list.tasks]
-                                .map((task, index) => ({ ...task, renderIndex: index + 1 }))
-                        }
-                    } else return list;
-
-                })
-            }
 
         case DELETE_TASK:
             return {
@@ -170,6 +191,7 @@ const reducer = (state = initialState, action) => {
                 })
             }
 
+
         default: return state;
     }
 
@@ -177,37 +199,159 @@ const reducer = (state = initialState, action) => {
 
 export default reducer;
 
+
+// --------------------------- Lists Actions -----------------------------------
+
 const RESTORE_LISTS = 'RESTORE-LISTS';
-export const restoreLists = (lists) => ({ type: RESTORE_LISTS, lists })
+const restoreListsAC = (lists) => ({ type: RESTORE_LISTS, lists })
+
+export const restoreLists = () => (dispatch) => {
+    dispatch(listIsLoadingAC(true))
+    api.restoreLists()
+        .then( response => {
+            dispatch(restoreListsAC(response.data));
+            dispatch(listIsLoadingAC(false))
+        })
+}
+
+const LIST_IS_LOADING = 'LIST_IS_LOADING'
+const listIsLoadingAC = (value) => ({ type: LIST_IS_LOADING, value })
 
 const ADD_LIST = 'ADD_LIST';
-export const addList = (list) => ({type: ADD_LIST, list})
+const addListAC = (list) => ({type: ADD_LIST, list})
+
+
+export const addList = (title) => (dispatch) => {
+    dispatch(listIsLoadingAC(true))
+    api.addList(title)
+            .then(Response => {
+                dispatch(addListAC(Response.data.data.item))
+                dispatch(listIsLoadingAC(false))
+            })
+}
 
 const UPDATE_LIST_TITLE = 'UPDATE_LIST_TITLE';
-export const updateListTitle = (listId, title) => ({ type: UPDATE_LIST_TITLE, listId, title })
+const updateListTitleAC = (listId, title) => ({ type: UPDATE_LIST_TITLE, listId, title })
+
+export const updateListTitle = (listId, title) => (dispatch) => {
+    dispatch(listInProcessAC(listId, 'titleUpdating', true))
+    api.updateListTitle(listId, title)
+        .then(Response => {
+            if (Response.data.resultCode === 0) {
+                dispatch(updateListTitleAC(listId, title))
+                dispatch(listInProcessAC(listId, 'titleUpdating', false))
+            }
+        })
+}
+
+const LIST_IN_PROCESS = 'LIST_IN_PROCESS'
+const listInProcessAC = (listId, process, value) => ({type: LIST_IN_PROCESS, listId, process, value})
 
 const DELETE_LIST = 'DELETE_LIST';
-export const deleteList = (listId) => ({type: DELETE_LIST, listId})
+const deleteListAC = (listId) => ({type: DELETE_LIST, listId})
+
+export const deleteList = (listId) => (dispatch) => {
+    dispatch(listInProcessAC(listId, 'listDeliting', true))
+    api.deleteList(listId)
+            .then(() => {
+                dispatch(deleteListAC(listId))
+                dispatch(listInProcessAC(listId, 'listDeliting', false))
+            })
+}
+
+
+const SET_FILTER_VALUE = 'SET_FILTER_VALUE'
+const setFilterValueAC = (listId, value) => ({type: SET_FILTER_VALUE, listId, value})
+
+export const setFilterValue = (listId, value) => async (dispatch) => {
+    dispatch( setFilterValueAC(listId, value) )
+}
+
+
+// --------------------------- Tasks Actions -----------------------------------
+
 
 const RESTORE_TASKS = 'RESTORE_TASKS';
-export const restoreTasks = (listId, tasks, totalCount) => ({ type: RESTORE_TASKS, listId, tasks, totalCount })
+const restoreTasksAC = (listId, tasks, totalCount) => ({ type: RESTORE_TASKS, listId, tasks, totalCount })
+
+export const restoreTasks = (listId) => (dispatch) => {
+    api.getTasks(listId)
+            .then(Response => {
+                dispatch(restoreTasksAC(listId, Response.data.items, Response.data.totalCount))
+            })
+}
 
 const SET_TASKS_PAGE = 'SET_TASKS_PAGE';
 export const setTasksPage = (listId, page, tasks, totalCount) =>
     ({ type: SET_TASKS_PAGE, listId, page, tasks, totalCount })
 
-const SET_FLTR_TASKS_PAGE = 'SET_FLTR_TASKS_PAGE';
-export const setFltrTasksPage = (listId, page, tasks, completed) =>
-    ({ type: SET_FLTR_TASKS_PAGE, listId, page, tasks, completed })
 
-const ADD_TASK = 'ADD_TASK';
-export const addTask = (task) => ({type: ADD_TASK, task})
+// set tasks page on "All" filter mode
+export const setAllTasksPage = (listId, page) => (dispatch) => {
+    return api.getTasksOnPage(listId, page)
+        .then(Response => {
+            const { items: tasks, totalCount } = Response.data;
+            dispatch(setTasksPage(listId, page, tasks, totalCount))
+        })
+}
+
 
 const DELETE_TASK = 'DELETE_TASK';
 export const deleteTask = (listId, taskId, page) => ({ type: DELETE_TASK, listId, taskId, page })
 
+const UPDATE_TASK = 'UPDATE_TASK';
+export const updateTask = (task) => ({type: UPDATE_TASK, task })
+
+
+
+// ------------------------- Add Tasks Actions --------------------------
+
+export const addTask = (listId, title) => (dispatch) => {
+    dispatch(listInProcessAC(listId, 'taskIsAdding', true))
+    return api.addTask(listId, title)
+        .then((Response) => {
+            if (Response.data.resultCode === 0) {
+                dispatch(setAllTasksPage(listId, 1))
+                    .then(() => {
+                        dispatch(setFilterValue(listId, 'All'))
+                        dispatch(listInProcessAC(listId, 'taskIsAdding', false))
+                    })
+            }
+        })
+}
+
+
+export const addTaskActive = (listId, title) => (dispatch) => {
+    dispatch( listInProcessAC(listId, 'taskIsAdding', true) )
+    return api.addTask(listId, title)
+        .then(Response => {
+            if (Response.data.resultCode === 0) {
+                api.getAllTasks(listId)
+                    .then((Response) => {
+                        const completed = false;
+                        const tasks = Response.data.items;
+                        dispatch( setFltrTasksPage(listId, 1, tasks, completed) )
+                        dispatch( listInProcessAC(listId, 'taskIsAdding', false) )
+                    })
+            }
+        })
+}
+
+
+// ------------------------ Filtered Tasks Actions ----------------------
+
+const SET_FLTR_TASKS_PAGE = 'SET_FLTR_TASKS_PAGE';
+export const setFltrTasksPage = (listId, page, tasks, completed) =>
+    ({ type: SET_FLTR_TASKS_PAGE, listId, page, tasks, completed })
+
 const DELETE_FLTR_TASK = 'DELETE_FLTR_TASK';
 export const deleteFltrTask = (listId, taskId, page) => ({ type: DELETE_FLTR_TASK, listId, taskId, page })
 
-const UPDATE_TASK = 'UPDATE_TASK';
-export const updateTask = (task) => ({type: UPDATE_TASK, task })
+// set tasks page on "Active" or "Completed" filter mode
+export const setFilteredPage = (listId, page, completed) => (dispatch) => {
+    return api.getAllTasks(listId)
+        .then(Response => {
+            const tasks = Response.data.items;
+            dispatch(setFltrTasksPage(listId, page, tasks, completed))
+        })
+}
