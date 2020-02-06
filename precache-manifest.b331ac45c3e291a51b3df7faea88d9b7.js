5514786cbdaebfbeb6640a25e22c6251a6904add
SerejKa-lab <seruogacom@mail.ru>
2020-02-06 17:06:09 +0300

    Updates

---
+++ precache-manifest.b331ac45c3e291a51b3df7faea88d9b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf1d0e595eba3d73316556476447069",
+    "url": "/TasksOrganizer/index.html"
+  },
+  {
+    "revision": "d22794fe3a46b3865841",
+    "url": "/TasksOrganizer/static/css/main.ae0dc146.chunk.css"
+  },
+  {
+    "revision": "3390b9646a214f8f9474",
+    "url": "/TasksOrganizer/static/js/2.c03d9f19.chunk.js"
+  },
+  {
+    "revision": "d22794fe3a46b3865841",
+    "url": "/TasksOrganizer/static/js/main.8b799827.chunk.js"
+  },
+  {
+    "revision": "02a729c17df90987bd39",
+    "url": "/TasksOrganizer/static/js/runtime~main.7ff27414.js"
+  },
+  {
+    "revision": "6d8795b6c283d050a6c4dd5f59eb8a21",
+    "url": "/TasksOrganizer/static/media/book.6d8795b6.png"
+  },
+  {
+    "revision": "fbcb2c9d5eded7eff091f3df1f8d4a7e",
+    "url": "/TasksOrganizer/static/media/navigation.fbcb2c9d.jpg"
+  }
+]);
