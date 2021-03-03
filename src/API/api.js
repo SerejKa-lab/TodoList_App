9e48b0f36a85a41601dbd3ce862fc5a98eafb203
Sergey Kashko <kashko.s.a@yandex.by>
2021-03-03 09:04:26 +0300

    API-KEY fix

--- src/API/api.js
+++ src/API/api.js
@@ -1,59 +1,59 @@
-import Axios from 'axios';
-
+import Axios from "axios";
 
 const instance = Axios.create({
-    baseURL: 'https://social-network.samuraijs.com/api/1.1/todo-lists',
-    withCredentials: true,
-    headers: { 'API-KEY': '077e296e-5c11-448c-9cff-a20ca6e66ed1' }
-})
+  baseURL: "https://social-network.samuraijs.com/api/1.1/todo-lists",
+  withCredentials: true,
+  headers: { "API-KEY": "077e296e-5c11-448c-9cff-a20ca6e66ed1" },
+});
 
 export const api = {
+  restoreLists() {
+    return instance.get("");
+  },
 
-    restoreLists() {
-        return instance.get('')
-    },
+  updateListTitle(listId, title) {
+    return instance.put(`/${listId}`, { title });
+  },
 
-    updateListTitle(listId, title) {
-        return instance.put(`/${listId}`, { title })
-    },
+  addList(title) {
+    return instance.post("", { title });
+  },
 
-    addList(title) {
-        return instance.post('', { title })
-    },
+  reorderList(listId, putAfterItemId) {
+    return instance.put(`/${listId}/reorder`, { putAfterItemId });
+  },
 
-    reorderList(listId, putAfterItemId) {
-        return instance.put(`/${listId}/reorder`, { putAfterItemId })
-    },
+  deleteList(listId) {
+    return instance.delete(`/${listId}`);
+  },
 
-    deleteList(listId) {
-        return instance.delete(`/${listId}`)
-    },
+  getTasks(listId, count = 10) {
+    return instance.get(`/${listId}/tasks?count=${count}`);
+  },
 
-    getTasks(listId, count = 10) {
-        return instance.get(`/${listId}/tasks?count=${count}`)
-    },
+  getAllTasks(listId) {
+    return instance.get(`/${listId}/tasks?count=100`);
+  },
 
-    getAllTasks(listId) {
-        return instance.get(`/${listId}/tasks?count=100`)
-    },
+  getTasksOnPage(listId, page = 1) {
+    return instance.get(`/${listId}/tasks?count=10&page=${page}`);
+  },
 
-    getTasksOnPage(listId, page = 1) {
-        return instance.get(`/${listId}/tasks?count=10&page=${page}`)
-    },
+  addTask(listId, title) {
+    return instance.post(`/${listId}/tasks`, { title });
+  },
 
-    addTask(listId, title) {
-        return instance.post(`/${listId}/tasks`, { title })
-    },
+  updateTask(listId, taskId, dataObj) {
+    return instance.put(`/${listId}/tasks/${taskId}`, { ...dataObj });
+  },
 
-    updateTask(listId, taskId, dataObj) {
-        return instance.put(`/${listId}/tasks/${taskId}`, { ...dataObj })
-    },
+  deleteTask(listId, taskId) {
+    return instance.delete(`/${listId}/tasks/${taskId}`);
+  },
 
-    deleteTask(listId, taskId) {
-        return instance.delete(`/${listId}/tasks/${taskId}`)
-    },
-
-    reorderTask(listId, taskId, putAfterItemId) {
-        return instance.put(`/${listId}/tasks/${taskId}/reorder`, { putAfterItemId })
-    }
-}
+  reorderTask(listId, taskId, putAfterItemId) {
+    return instance.put(`/${listId}/tasks/${taskId}/reorder`, {
+      putAfterItemId,
+    });
+  },
+};
